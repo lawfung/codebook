08f195d9cab1829ef86fec7a1c66a0d59cbc33b7
lawfung <lawfung60@gmail.com>
2020-08-04 13:21:24 +0800

    costflow

--- flow/costflow.cpp
+++ flow/costflow.cpp
@@ -1,41 +1,58 @@
-struct Cost_Flow {
-    void add_edge(int from, int to, int cap, int cost) {
-        G[from].push_back(Edge(to, cap, SZ(G[to]), cost));
-        G[to].push_back(Edge(from, 0, SZ(G[from]) - 1, -cost));
-    }
-    pii solve() {
-        int flow = 0, cost = 0;
-        while (true) {
-            fill(dis, dis + N, INF); dis[s] = 0;
-            queue<int> que; que.push(s);
-            while (!que.empty()) {
-                int t = que.front(); que.pop();
-                in_que[t] = false; int id = 0;
-                for (Edge e: G[t]) {
-                    if (e.cap > 0 && dis[e.to] > dis[t] + e.cost) {
-                        dis[e.to] = dis[t] + e.cost;
-                        pre[e.to] = t; pre_id[e.to] = id;
-                        if (!in_que[e.to]) {
-                            in_que[e.to] = true; que.push(e.to);
-                        }
-                    }
-                    ++id;
-                }
-            }
-            if (dis[t] == INF) break;
-            int mn_flow = INF;
-            for (int i = t; i != s; i = pre[i]) {
-                mn_flow = min(mn_flow, G[ pre[i] ][ pre_id[i] ].cap);
-            }
-            flow += mn_flow;
-            cost += mn_flow * dis[t];
-            for (int i = t; i != s; i = pre[i]) {
-                G[ pre[i] ][ pre_id[i] ].cap -= mn_flow;
-                G[ i ][ G[ pre[i] ][ pre_id[i] ].rev ].cap += mn_flow;
-            }
-        }
-        return make_pair(flow, cost);
-    }
-} solver;
-
-
+const ll N = 5e2 + 5;
+struct MCFlow{
+	struct edge{
+		int t, r;
+		ll cap, cos;
+		edge(int _t, int _r, ll _cp, ll _co) : t(_t), r(_r), cap(_cp), cos(_co){}
+	};
+	int n, S, T;
+	vector<edge> adj[N];
+	MCFlow(int _n,int _s,int _t) : n(_n), S(_s), T(_t) {
+		for(int i = 0; i < n; ++ i)
+			adj[i].clear();
+	}
+	void add(int s, int t, ll cap, ll cos){
+		adj[s].eb(t, SZ(adj[t])  , cap, cos);
+		adj[t].eb(s, SZ(adj[s])-1, 0  , -cos);
+	}
+	pll flow(){
+		ll tc = 0, tf = 0, dis[N];
+		int inq[N], pre[N], prE[N];
+		while(1){
+			memset(dis, INF, n * 8);
+			memset(inq, 0  , n * 4);
+			queue<int> qu;
+			qu.push(S);
+			inq[S] = 1;
+			dis[S] = 0;
+			while(SZ(qu)){
+				int now = qu.front();
+				qu.pop();
+				inq[now] = 0;
+				for(int i = 0; i < SZ(adj[now]); ++i){
+					auto e = adj[now][i];
+					if(e.cap && dis[now] + e.cos < dis[e.t]){
+						dis[e.t] = dis[now] + e.cos;
+						pre[e.t] = now;
+						prE[e.t] = i;
+						if(!inq[e.t]){
+							qu.push(e.t);
+							inq[e.t] = 1;
+						}
+					}
+				}
+			}
+			if(dis[T] == INF)	break;
+			ll mi = INF;
+			for(int now = T; now != S; now = pre[now])
+				mi = min(mi, adj[pre[now]][prE[now]].cap);
+			for(int now = T; now != S; now = pre[now]){
+				adj[pre[now]][prE[now] ]            .cap-=mi;
+				adj[now][adj[pre[now]][prE[now]].r ].cap+=mi;
+			}
+			tc += mi * dis[T];
+			tf += mi;
+		}
+		return pll(tf, tc);
+	}
+};
