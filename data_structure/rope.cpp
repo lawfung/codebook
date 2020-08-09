8bd78aea4806ff3d477b355f11d46ed2f5198a48
lawfung <lawfung60@gmail.com>
2020-08-09 08:09:39 +0800

    rope

--- data_structure/rope.cpp
+++ data_structure/rope.cpp
@@ -1,9 +1,16 @@
 #include <ext/rope>
 using namespace __gnu_cxx;
-
-rope<int> *p[N],*sz[N];  //use merge by size
-int pp[N],szz[N];
-
-int ret = p[ver]->at(x);
-p[ver]->replace(x,ret);
-p[0] = new rope<int>(pp,pp+n+1);
+int main() {
+	rope<int> v;	// can be cout directly if it's char
+	rope<int> v1(v);
+	rope<int> v2(arr, arr + 10); //int arr[100];
+	v.find(3); // return the first positoin of 3
+	v.push_back(4);
+	v.insert(pos, s); // pos can be iterator, integer. s can be rope, int, array 
+	v.replace(pos, s); // same as insert
+	v.erase(pos, len); // or v.erase(it1, it2)
+	v2 = v.substr(pos, len); // same as erase
+	v.copy(pos, len, arr); // int arr[100]; (pos, len) can be omitted
+	v[0], v[1]
+	auto it1 = v.mutable_begin(), it2 = v.mutable_end();
+}
