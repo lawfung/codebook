0d46afbbc5857332c0863e7090a079b262a0090e
lawfung <lawfung60@gmail.com>
2020-10-09 22:41:38 +0800

    centroid DC

--- graph/Centroid_Decomposition.cpp
+++ graph/Centroid_Decomposition.cpp
@@ -1,133 +1,83 @@
-#include <iostream>
-#include <stdio.h>
-#include <vector>
-#include <cstring>
-using namespace std;
+const int Mlg = __lg(MAX) + 2;
 
-typedef long long Meruru;
-const int MAX_N = 1e5 + 6;
-const int MAX_P = 18;
-
-struct Edge {
+struct edge {
 	int to,weight;
+	edge(int _to,int _w):to(_to),weight(_w){}
 };
 
-Edge MP(int _to,int _weight) {
-	return Edge{_to,_weight};
-}
-
-vector<Edge> edg[MAX_N];
-Meruru dis[MAX_P][MAX_N];
-bool visit[MAX_N];
-int sz[MAX_N];
-int mx[MAX_N];
+vector<edge> edg[MAX];
 
 struct Cen {
-	Meruru minus;
-	Meruru val;
-	int p;
-	int sz;
-	int depth;
-} cen[MAX_N];
+	ll val;
+	int p, sz, dep;
+	Cen(){}
+	Cen(int _p,int _d):val(0),p(_p),sz(0),dep(_d){}
+} cen[MAX];
+ll dis[Mlg][MAX];
 
-Cen MP_cen(int _p,int _depth) {
-	return Cen{0,0,_p,0,_depth};
-}
-
+bool visit[MAX];
 vector<int> v;
-
-void get_cen(int id) {
+int sz[MAX], mx[MAX];
+void dfs_sz(int id) {
 	visit[id]=1;
 	v.push_back(id);
 	sz[id]=1;
 	mx[id]=0;
-	for (Edge i:edg[id]) {
+	for (edge i:edg[id]) {
 		if (!visit[i.to]) {
-			get_cen(i.to);
+			dfs_sz(i.to);
 			mx[id] = max(mx[id],sz[i.to]);
 			sz[id] += sz[i.to];
 		}
 	}
 }
-
-void get_dis(int id,int cen_depth,Meruru weight) {
-	dis[cen_depth][id] = weight;
+void dfs_dis(int id,int cen_dep,ll weight) {
+	dis[cen_dep][id] = weight;
 	visit[id]=1;
-	for (Edge i:edg[id]) {
-		if (!visit[i.to]) {
-			get_dis(i.to,cen_depth,weight+i.weight);
-		}
-	}
+	for (edge i:edg[id])
+		if (!visit[i.to])
+			dfs_dis(i.to,cen_dep,weight+i.weight);
 }
-
-void dfs(int id,int cen_depth,int p) {
-	get_cen(id);
+void build(int id,int cen_dep,int p) {
+	dfs_sz(id);
 	int nn=v.size();
 	int ccen=-1;
 	for (int i:v) {
-		if (max(nn-sz[i],mx[i]) <= nn/2) {
+		if (max(nn-sz[i],mx[i])*2 <= nn)
 			ccen=i;
-		}
 		visit[i]=0;
 	}
-	get_dis(ccen,cen_depth,0);
-	for (int i:v) {
-		visit[i]=0;
-	}
+	dfs_dis(ccen,cen_dep,0);
+	for (int i:v)   visit[i]=0;
 	v.clear();
 	visit[ccen]=1;
-	cen[ccen] = MP_cen(p,cen_depth);
-	for (Edge i:edg[ccen]) {
-		if (!visit[i.to]) {
-			dfs(i.to,cen_depth+1,ccen);
-		}
+	cen[ccen] = Cen(p,cen_dep);
+	for (edge i:edg[ccen])
+		if (!visit[i.to])
+			build(i.to,cen_dep+1,ccen);
+}
+
+void add(int id, int d) {
+	for(int p=id;p!=-1;p=cen[p].p){
+		cen[p].val += dis[cen[p].dep][id]*d;
+		cen[p].val -= dis[cen[p].dep-1][id]*d;
+		cen[p].sz += d;
 	}
 }
 
-void add(int id) {
-	int p=id;
-	while (p!=-1) {
-		cen[p].val += dis[cen[p].depth][id];
-		cen[p].sz++;
-		cen[p].minus += dis[cen[p].depth-1][id];
-		p=cen[p].p;
-	}
-}
-
-Meruru query(int id) {
-	int p=id;
-	Meruru ret=0;
-	int szz=0;
-	while (p!=-1) {
-		ret += (cen[p].val - cen[p].minus);
-		ret += (cen[p].sz - szz)*dis[cen[p].depth][id];
-		szz = cen[p].sz;
-		p=cen[p].p;
+ll query(int id) {
+	ll ret=0;
+	int pre_sz=0;
+	for(int p=id;p!=-1;p=cen[p].p){
+		ret += cen[p].val;
+		ret += (cen[p].sz - pre_sz)*dis[cen[p].dep][id];
+		pre_sz = cen[p].sz;
 	}
 	return ret;
 }
-
-int main () {
-	int n,q;
-	scanf("%d %d",&n,&q);
-	for (int i=1;n>i;i++) {
-		int a,b,c;
-		scanf("%d %d %d",&a,&b,&c);
-		a++;
-		b++;
-		edg[a].push_back(MP(b,c));
-		edg[b].push_back(MP(a,c));
-	}
-	dfs(1,1,-1);
-	memset(visit,0,sizeof(visit));
-	while (q--) {
-		int a,b;
-		scanf("%d %d",&a,&b);
-		b++;
-		if (a==1 && !visit[b]) {
-			add(b);
-			visit[b]=1;
-		}
-		else if (a==2)printf("%lld\n",query(b));
-	}
-}
+// edg[u].push_back(edge(v,w));
+// edg[v].push_back(edge(u,w));
+// memset(visit,0,sizeof(visit));
+// build(1,1,-1);
+// add(u, d)
+// query(u)
