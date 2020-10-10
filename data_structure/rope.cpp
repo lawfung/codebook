d8e1323b86551c76122ffb7b6ff29b76bade1169
lawfung <lawfung60@gmail.com>
2020-10-11 00:33:52 +0800

    Palindromic_Tree

--- data_structure/rope.cpp
+++ data_structure/rope.cpp
@@ -5,9 +5,10 @@ int main() {
 	rope<int> v1(v);
 	rope<int> v2(arr, arr + 10); //int arr[100];
 	v.find(3); // return the first positoin of 3
-	v.push_back(4);
+	v.push_back(4); v.pop_back();
+	//append not for iterator
 	v.insert(pos, s); // pos can be iterator, integer. s can be rope, int, array 
-	v.replace(pos, s); // same as insert
+	v.replace(pos, len, s); // (pos, len) can be (it1, it2). s is same as insert.
 	v.erase(pos, len); // or v.erase(it1, it2)
 	v2 = v.substr(pos, len); // same as erase
 	v.copy(pos, len, arr); // int arr[100]; (pos, len) can be omitted
